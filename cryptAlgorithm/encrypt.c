cae7b367431bb90d0e89890043a950754af68e48
Ingenieria.inversa <jdg@member.fsf.org>
2016-07-28 12:46:24 +0200

    Update encrypt.c

--- cryptAlgorithm/encrypt.c
+++ cryptAlgorithm/encrypt.c
@@ -8,8 +8,7 @@ void uso_err(char *bin) {
 }
 
 int main(int argc, char **argv) {
-
-	int i, z;
+	int i, z, x;
 
 	switch (argc) {
 	case 1:
@@ -18,12 +17,25 @@ int main(int argc, char **argv) {
 	default:
 		for (i = 1; i < argc; i++) {
 			char *cadena = argv[i];
-			for (z = 0; z < strlen(cadena); z++) {
-				printf("%c : %02X\n", cadena[z], cadena[z]);
+			for (z = 0, x = 1; z < strlen(cadena); z++, x++) {
+				printf("%c : %d : %.3d : %.3d : %02X : %02X\n", cadena[z], x,
+						cadena[z], cadena[z] - z, cadena[z], cadena[z] - x);
 			}
 			if (i != argc - 1)
-				printf("%c : %02X\n", 32, 32);
+				printf("%c : %d : %.3d : %.3d : %02X : %02X\n", 32, x, 32,
+						32 - x, 32, 32 - x);
 		}
+		printf("\n");
+
+		for (i = 1; i < argc; i++) {
+			char *cadena = argv[i];
+			for (z = 0, x = 1; z < strlen(cadena); z++, x++) {
+				printf("%02X", cadena[z] - x);
+			}
+			if (i != argc - 1)
+				printf("%02X", 32 - x);
+		}
+		printf("\n");
 	}
 
 	return 0;
