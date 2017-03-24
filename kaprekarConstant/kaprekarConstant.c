1300088f88bb55893d7ee1c3f082cadfe4a3195d
Ingenieria.inversa <jdg@member.fsf.org>
2017-03-24 08:43:28 +0100

    Update kaprekarConstant.c

--- kaprekarConstant/kaprekarConstant.c
+++ kaprekarConstant/kaprekarConstant.c
@@ -1,41 +1,5 @@
-/*
- * kaprekarConstant v0.01
- * Copyleft - 2016  Javier Dominguez Gomez
- * Written by Javier Dominguez Gomez <jdg@member.fsf.org>
- * GnuPG Key: 6ECD1616
- * Madrid, Spain
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * Compilation: gcc -std=gnu99 -O0 -g3 -Wall -c -o kaprekarConstant.o "kaprekarConstant.c"
- *              gcc -o kaprekarConstant kaprekarConstant.o
- *
- * Usage:       ./kaprekarConstant
- */
- 
 #include<stdio.h>
 #include<stdbool.h>
-
-/* Procedimiento para ordenar dos numeros. */
-void swap(int *x, int *y) {
-	if (*x < *y) {
-		int aux;
-		aux = *x;
-		*x = *y;
-		*y = aux;
-	}
-}
 
 /* Funcion que devuelve la longitud de un int. */
 int LongitudInt(int n) {
@@ -99,14 +63,7 @@ int main() {
 		scanf("%d", &n);
 	} while (CompruebaDigitos(n) == false);
 
-	x = n;
-	y = OrdenacionAscendenteInt(x);
-
-	/* Se asigna a x el numero mayor y a y el menor. */
-	swap(&x, &y);
-
-	kaprekar = x - y;
-	printf("%d - %d = %d\n", x, y, kaprekar);
+	kaprekar = n;
 
 	do {
 		if (kaprekar == 0) {
