2b66eb71c123c8641634d4049f383a8da478b673
Ingenieria.inversa <jdg@member.fsf.org>
2017-03-24 14:04:51 +0100

    Update kaprekarConstant.c

--- kaprekarConstant/kaprekarConstant.c
+++ kaprekarConstant/kaprekarConstant.c
@@ -1,3 +1,29 @@
+/*
+ * constanteKaprekar v0.01
+ * Copyleft - 2016  Javier Dominguez Gomez
+ * Written by Javier Dominguez Gomez <jdg@member.fsf.org>
+ * GnuPG Key: 6ECD1616
+ * Madrid, Spain
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Compilation: gcc -std=gnu99 -O0 -g3 -Wall -c -o constanteKaprekar.o "constanteKaprekar.c"
+ *              gcc -o constanteKaprekar constanteKaprekar.o
+ *
+ * Usage:       ./constanteKaprekar
+ */
+
 #include<stdio.h>
 #include<stdbool.h>
 
