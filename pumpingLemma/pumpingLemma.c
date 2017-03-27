d6f0616342e4b1359fda6e6cb6eb08c739ed8ccf
Ingenieria.inversa <jdg@member.fsf.org>
2017-03-27 16:31:50 +0200

    Update pumpingLemma.c

--- pumpingLemma/pumpingLemma.c
+++ pumpingLemma/pumpingLemma.c
@@ -35,7 +35,7 @@
 /* Variables para la constante de bombeo y para subcadenas de w. */
 int n, x, y;
 
-int main(int argC, char *argV[]) {
+int main(int argc, char *argv[]) {
 
 	/* Se asigna memoria para la cadena w y se comprueba si esta bien. */
 	char *w = malloc(MAX_LONG_W);
@@ -49,7 +49,9 @@ int main(int argC, char *argV[]) {
 	/* Guarda la cadena w, con limite de longitud. */
 	fgets(w, MAX_LONG_W, stdin);
 
+	/* Se obtiene la logitud de la cadena de entrada. */
 	int longitudW = strlen(w) - 1;
+
 	/* Elimina salto de linea del final, si existiera. */
 	if ((longitudW + 1 > 0) && (w[longitudW] == '\n')) {
 		w[longitudW] = '\0';
@@ -60,12 +62,46 @@ int main(int argC, char *argV[]) {
 	/* Guarda la constante de bombeo.*/
 	scanf("%d", &n);
 
+	/* Se asigna memoria para la subcadena xy y se comprueba si esta bien. */
+	char *xy = malloc(sizeof(n));
+	if (xy == NULL) {
+		printf("ERROR: Longitud de cadena.\n");
+		return 1;
+	}
+
 	/* 1. Se comprueba si |w|>=n. */
 	if (longitudW >= n) {
-		printf("La cadena \"%s\" cumple la regla |w|>=n, ya que |%d|>=%d.\n", w,
-				longitudW, n);
+		printf("La cadena \"%s\" cumple la regla |w|>=n, ya que |%d|>=%d.\n\n",
+				w, longitudW, n);
 
-		/* 2. Se descompone la cadena w en tres subcadenas xyz. */
+		/* 2. Se descompone la cadena w en tres subcadenas x,y y z. */
+		int i;
+		for (i = 0; i < longitudW; i++) {
+			printf("w[%d] = %c\n", i, w[i]);
+		}
+
+		/* Copio los n primeros simbolos de la cadena w en la subcadena xy. */
+		strncpy(xy, w, n);
+
+		/* Elimino el ultimo caracter de la cadena xy. */
+		xy[n] = 0;
+
+		/* Se obtiene la logitud de la cadena xy. */
+		int longitudXY = strlen(xy);
+
+		/* Se asigna memoria para la subcadena z y se comprueba si esta bien. */
+		char *z = malloc(longitudW - longitudXY);
+		if (z == NULL) {
+			printf("ERROR: Longitud de cadena.\n");
+			return 1;
+		}
+
+		strcpy(z, &w[n]);
+
+		printf("\nn = %d\n", n);
+		printf("w = %s\n", w);
+		printf("|w|>=n  \t -> |%d|>=%d\n", longitudW, n);
+		printf("w = xyz \t -> %s = %s+%s\n", w, xy, z);
 
 		/* 2.1 Se comprueba que |xy|<=n. */
 
@@ -73,17 +109,14 @@ int main(int argC, char *argV[]) {
 
 		/* 2.3 Se comprueba que para todo k>=0 la cadena xy^{k}z tambien pertenece a L. */
 
+		free(w);
+		free(xy);
+		free(z);
+
 	} else {
 		printf("La cadena \"%s\" no cumple la regla |w|>=n, ya que |%d|<%d.\n",
 				w, longitudW, n);
 	}
 
-//	int i;
-//	for(i = 0;i < strlen(w);i++) {
-//		printf("Posicion %2d del array = %c\n", i, w[i]);
-//	}
-
-	/* Libera memoria y termina el programa. */
-	free(w);
 	return 0;
 }
